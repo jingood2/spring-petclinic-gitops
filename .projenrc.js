ea50f70df2ef1de16693bf32ea17bb80eab9de78
jingood2 <jingood2@gmail.com>
2022-10-11 16:56:06 +0900

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,12 @@
+const { cdk8s } = require('projen');
+const project = new cdk8s.Cdk8sTypeScriptApp({
+  cdk8sVersion: '2.3.33',
+  defaultReleaseBranch: 'main',
+  name: 'spring-petclinic-gitops',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
